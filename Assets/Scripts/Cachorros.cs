45915ef8d1d1644182d00d30a06a99ebbacbc3d3
pedro_ga_santos <pedro_ga_santos@estudante.sesisenai.org.br>
2026-05-19 21:31:06 -0300

    atributo sono add

--- Assets/Scripts/Cachorros.cs
+++ Assets/Scripts/Cachorros.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class Cachorros : MonoBehaviour
 {
     public string nome;
+    public bool sono = false;
 
     private void Start()
     {
        Cachorros pitbull = new Cachorros();
          pitbull.nome = "Rex";
+         pitbull.sono = true;
     }
 }
