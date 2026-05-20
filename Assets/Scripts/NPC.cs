0afc6f2fa67cb7aff17739e883de8b8c4ea735df
pedro_ga_santos <pedro_ga_santos@estudante.sesisenai.org.br>
2026-05-19 21:39:15 -0300

    jonas criado e add infos

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public string nome;
+    public bool podeFalar;
+    public int idade;
+
     void Start()
     {
-        
+        NPC jonas = new NPC();
+        jonas.nome = "Jonas";
+        jonas.podeFalar = true;
+        jonas.idade = 30;
+
     }
 
   
