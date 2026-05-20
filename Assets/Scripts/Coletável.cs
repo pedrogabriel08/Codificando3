e2e9bd78e28e8030db9714cf07f6b3e90cf28ef2
pedro_ga_santos <pedro_ga_santos@estudante.sesisenai.org.br>
2026-05-20 19:59:45 -0300

    coletaveis add

--- Assets/Scripts/Coletável.cs
+++ Assets/Scripts/Coletável.cs
@@ -2,13 +2,26 @@ using UnityEngine;
 
 public class Coletável : MonoBehaviour
 {
-
+    public string nome;
+    public int valor;
+    public bool raro;
+    public string descrição;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Coletável moeda = new Coletável();
+        moeda.nome = "Moeda de Ouro";
+        moeda.valor = 100;
+        moeda.raro = false;
+        moeda.descrição = "Uma moeda de ouro brilhante, valiosa mas comum.";
+
+        Coletável gema = new Coletável();
+        gema.nome = "Gema Mística";
+        gema.valor = 500;
+        gema.raro = true;
+        gema.descrição = "Uma gema rara e poderosa, cobiçada por aventureiros.";
 
 
     }
 
-   
 }
