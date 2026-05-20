ac26cb98bafc84b1c7d4a691465e6143225d38b9
pedro_ga_santos <pedro_ga_santos@estudante.sesisenai.org.br>
2026-05-20 20:31:09 -0300

    void atacar e if e else criados

--- Assets/Scripts/Inimigos.cs
+++ Assets/Scripts/Inimigos.cs
@@ -5,6 +5,17 @@ public class Inimigos : MonoBehaviour
     public int dano;
     public int vida;
     public bool Special;
+
+    void Atacar()
+    {
+     
+        if (Special) {
+            Debug.Log("Ataque Especial");
+        }
+        else
+        {
+            Debug.Log("Ataque Normal");
+        }   
     void Start()
     {
         
